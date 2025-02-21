313de3fe8e3d60e3ce50731e12ff0f406acfe65a
Barroso18 <david.barrosomartinez@gmail.com>
2025-02-21 13:47:17 +0100

    Continuacion

--- SecuringWeb/src/main/java/goya/daw2/ud7/MvcConfig.java
+++ SecuringWeb/src/main/java/goya/daw2/ud7/MvcConfig.java
@@ -9,7 +9,7 @@ public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/hola").setViewName("hola");
 		registry.addViewController("/login").setViewName("login");
 	}
 
